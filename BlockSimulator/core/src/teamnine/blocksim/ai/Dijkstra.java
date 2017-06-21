20b96bb0da5a2f955ffd35558349ce499c28c33f
martadavila24 <marta.davila.24@gmail.com>
2017-06-21 11:05:22 +0200

    worgnfekasc

--- BlockSimulator/core/src/teamnine/blocksim/ai/Dijkstra.java
+++ BlockSimulator/core/src/teamnine/blocksim/ai/Dijkstra.java
@@ -35,65 +35,68 @@ public class Dijkstra
 	public void completeFinalList(PriorityQueue<DistanceBlock> listToReduce)
     {
         DistanceBlock position = listToReduce.poll();
-        finalList.add(position);
-        DistanceBlock[] neighbours = position.getNeighbours();
-        int[] weights = position.getWeights();
-        
-        //checks if the position is the target
-        if((position.getX() == target.getX()) && (position.getZ() == target.getZ()))
-        {
-        	return;
-        }
-        
-        else if (neighbours != null)
-        {
-            for (int i = 0; i < neighbours.length; i++)
-            {
-            	DistanceBlock neig = neighbours[i];
-            	DistanceBlock neighbour = null;
-            	boolean containsNeig = false;
-            	
-            	ArrayList<DistanceBlock> array = new ArrayList<DistanceBlock>();
-            	int size = listToReduce.size();
-        		for (int m = 0; m < size; m++)
-        		{
-        			DistanceBlock object = listToReduce.poll();
-        			if((neig.getX() == object.getX()) && (neig.getZ() == object.getZ()))
-        			{
-        				for(int j= 0; j < array.size(); j++)
-        				{
-        					listToReduce.add(array.get(j));
-        				}
-        				neighbour = object;
-        				containsNeig = true;
-        				m = listToReduce.size();
-        			}
-        			else
-        			{
-        				array.add(object);
-        			}
-        		}
-        		if (!(containsNeig))
-        		{
-        			for(int j= 0; j < array.size(); j++)
-    				{
-    					listToReduce.add(array.get(j));
-    				}
-        		}	
-        		else
-        		{
-		            if (neighbour.getDistance() > (position.getDistance() + weights[i]))
-		            {
-		                 neighbour.setPrevious(position);
-		                 neighbour.setDistance(position.getDistance() + weights[i]);
-		            }
-		            listToReduce.add(neighbour);
-        		}
-            }
-            
+        //if (((((position.getHigh() * (position.getHigh()+1))/2)) <= numTargetBlocks) &&
+        		//((((position.getHigh() * (position.getHigh()+1))/2)) <= numRoboBlocks))
+        //{
+	        finalList.add(position);
+	        DistanceBlock[] neighbours = position.getNeighbours();
+	        int[] weights = position.getWeights();
+	        
+	        //checks if the position is the target
+	        if((position.getX() == target.getX()) && (position.getZ() == target.getZ()))
+	        {
+	        		return;
+	        }
+	        
+	        else if (neighbours != null)
+	        {
+	            for (int i = 0; i < neighbours.length; i++)
+		        {
+		            	DistanceBlock neig = neighbours[i];
+		            	DistanceBlock neighbour = null;
+		            	boolean containsNeig = false;
+		            	
+		            	ArrayList<DistanceBlock> array = new ArrayList<DistanceBlock>();
+		            	int size = listToReduce.size();
+		        		for (int m = 0; m < size; m++)
+		        		{
+		        			DistanceBlock object = listToReduce.poll();
+		        			if((neig.getX() == object.getX()) && (neig.getZ() == object.getZ()))
+		        			{
+		        				for(int j= 0; j < array.size(); j++)
+		        				{
+		        					listToReduce.add(array.get(j));
+		        				}
+		        				neighbour = object;
+		        				containsNeig = true;
+		        				m = listToReduce.size();
+		        			}
+		        			else
+		        			{
+		        				array.add(object);
+		        			}
+		        		}
+		        		if (!(containsNeig))
+		        		{
+		        			for(int j= 0; j < array.size(); j++)
+		    				{
+		    					listToReduce.add(array.get(j));
+		    				}
+		        		}	
+		        		else
+		        		{
+		        			if (neighbour.getDistance() > (position.getDistance() + weights[i]))
+			            {
+			                 neighbour.setPrevious(position);
+			                 neighbour.setDistance(position.getDistance() + weights[i]);
+			            }
+			            listToReduce.add(neighbour);
+		        		}
+	            }
+	       // }
             if (listToReduce.size() != 0)
             {
-            	completeFinalList(listToReduce);
+            		completeFinalList(listToReduce);
             }
         }
     }
